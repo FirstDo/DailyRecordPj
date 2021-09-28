d2cd6a8a569832ed207152c121f0d45d5ead4ab4
FirstDo <whqtkf12@naver.com>
2021-09-28 22:50:56 +0900

    Add CocoaPod & ScrollView & FSCalendar

--- DailyRecordProject/ViewController.swift
+++ DailyRecordProject/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import FSCalendar
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    //scrollView
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    //FSCalendar
+    let calendar: FSCalendar = {
+        let calendar = FSCalendar()
+        calendar.locale = Locale(identifier: "ko_KR")
+        calendar.appearance.headerMinimumDissolvedAlpha = 0.0
+        calendar.headerHeight = 40
+        calendar.appearance.headerTitleColor = .black
+        calendar.appearance.headerDateFormat = "YYYY년 M월"
+        calendar.appearance.headerTitleFont = UIFont.systemFont(ofSize: 24)
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        return calendar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        scrollViewSetting()
+        calendarSetting()
     }
+}
+
+//MARK: - scrollView
+extension ViewController {
+    func scrollViewSetting() {
+        view.addSubview(scrollView)
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        scrollView.addSubview(calendar)
+    }
+}
 
 
+extension ViewController: FSCalendarDelegate, FSCalendarDataSource {
+    func calendarSetting() {
+        calendar.delegate = self
+        calendar.dataSource = self
+        
+        calendar.heightAnchor.constraint(equalToConstant: 500).isActive = true
+        calendar.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 0).isActive = true
+        calendar.leadingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        calendar.trailingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        //calendar.bottomAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print("select: ",date)
+    }
+    
+    func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print("deselect: ",date)
+    }
 }
 
