de6828b41285806a985eb7d57c2b6899ef6d14b7
FirstDo <whqtkf12@naver.com>
2021-09-28 23:18:42 +0900

    add contentView

--- DailyRecordProject/ViewController.swift
+++ DailyRecordProject/ViewController.swift
@@ -28,15 +28,33 @@ class ViewController: UIViewController{
         return calendar
     }()
     
+    //ContentView
+    let contentView: UIView = {
+        let v = UIView()
+        v.backgroundColor = .red
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    
+    //label
+    let lb: UILabel = {
+        let lb = UILabel()
+        lb.text = "hi"
+        lb.translatesAutoresizingMaskIntoConstraints = false
+        return lb
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollViewSetting()
         calendarSetting()
+        contentViewSetting()
     }
 }
 
-//MARK: - scrollView
+//MARK: - scrollView, contentView, calendar
 extension ViewController {
+    
     func scrollViewSetting() {
         view.addSubview(scrollView)
         scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
@@ -45,27 +63,39 @@ extension ViewController {
         scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
         scrollView.addSubview(calendar)
     }
-}
-
-
-extension ViewController: FSCalendarDelegate, FSCalendarDataSource {
+    
     func calendarSetting() {
         calendar.delegate = self
         calendar.dataSource = self
         
-        calendar.heightAnchor.constraint(equalToConstant: 500).isActive = true
+        let calendarHeight = view.frame.size.height - 200
+        
+        calendar.heightAnchor.constraint(equalToConstant: calendarHeight).isActive = true
         calendar.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 0).isActive = true
         calendar.leadingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor).isActive = true
         calendar.trailingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        //calendar.bottomAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
     }
     
+    func contentViewSetting() {
+        scrollView.addSubview(contentView)
+        
+        contentView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: calendar.bottomAnchor, constant: 30).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -40).isActive = true
+    }
+}
+
+
+extension ViewController: FSCalendarDelegate, FSCalendarDataSource {
+    
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        print("select: ",date)
+        print("select")
     }
     
     func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        print("deselect: ",date)
+        print("deselect")
     }
 }
 
