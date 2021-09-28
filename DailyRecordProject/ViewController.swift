0fd902b008d6488b04631c1fdedd06d7d50f4f0b
FirstDo <whqtkf12@naver.com>
2021-09-28 21:16:12 +0900

    Initial Commit

---
+++ DailyRecordProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyRecordProject
+//
+//  Created by 김도연 on 2021/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
