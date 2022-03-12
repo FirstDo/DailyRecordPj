a5455027e0743e8b15a36a841ad8882d8986cee9
dudu <whqtkf12@naver.com>
2022-03-13 06:48:02 +0900

    refactor: UserDefaultSetting.swift -> UserDefaultKey.swift로 네이밍 변경 및 class -> enum 으로 변경

--- Shared/UserDefaultKey.swift
+++ Shared/UserDefaultKey.swift
@@ -1,17 +1,8 @@
-//
-//UserDefault key
-//
-
-import Foundation
-
-class UserDefaultKey {
-    //for ListView tap
+enum UserDefaultKey {
     static let listMonth = "listMonth"
     static let listYear = "listYear"
     
-    //for Keyboard Height
     static let keyboardHeight = "keyboardHeight"
     
-    //push switch
     static let switchState = "switchState"
 }
