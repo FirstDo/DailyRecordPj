f4462ca49ae2763a99d8fbd2e75bc94bf86efc80
dudu <whqtkf12@naver.com>
2022-03-13 07:27:02 +0900

    refactor: PushNotification.swift 내부의 notification등록함수들 모두 SceneDelegate로 이동 및 기존 파일 삭제

--- DailyRecordProject/SceneDelegate.swift
+++ DailyRecordProject/SceneDelegate.swift
@@ -24,12 +24,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillResignActive(_ scene: UIScene) { }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        UNUserNotificationCenter.current().getNotificationSettings { setting in
+        UNUserNotificationCenter.current().getNotificationSettings { [weak self] setting in
             switch setting.authorizationStatus {
             case .authorized:
-                reservePushNoti()
+                self?.reservePushNoti()
             case .denied:
-                removePushNoti()
+                self?.removePushNoti()
             default:
                 break
             }
@@ -38,4 +38,33 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidEnterBackground(_ scene: UIScene) { }
 }
+
+extension SceneDelegate {
+    private func reservePushNoti() {
+        let content = UNMutableNotificationContent()
+        
+        content.title = "하루기록"
+        content.body = "오늘의 하루를 기록하세요"
+        content.badge = 1
+        content.sound = UNNotificationSound.default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 22
+        dateComponents.minute = 0
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let request = UNNotificationRequest(identifier: "alarm", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { _ in }
+        UserDefaults.standard.set(true, forKey: UserDefaultKey.switchState)
+        NotificationCenter.default.post(name: .pushChanged, object: nil)
+    }
+
+    private func removePushNoti() {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UserDefaults.standard.set(false, forKey: UserDefaultKey.switchState)
+        NotificationCenter.default.post(name: .pushChanged, object: nil)
+    }
+}
+
 
