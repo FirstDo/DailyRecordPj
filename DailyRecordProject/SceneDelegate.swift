2186083e7b38b38f805485ed3a22f8ea995ab1f5
dudu <whqtkf12@naver.com>
2022-03-13 06:23:26 +0900

    Chore: SceneDelegate 주석제거 및 라인정리

--- DailyRecordProject/SceneDelegate.swift
+++ DailyRecordProject/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  DailyRecordProject
 //
-//  Created by 김도연 on 2021/09/28.
+//  Created by DuDu on 2021/09/28.
 //
 
 import UIKit
@@ -10,26 +10,18 @@ import UserNotifications
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
+    func sceneDidDisconnect(_ scene: UIScene) { }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        //set badgeNumber to zero
         UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
+    func sceneWillResignActive(_ scene: UIScene) { }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         UNUserNotificationCenter.current().getNotificationSettings { setting in
@@ -41,18 +33,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             default:
                 break
             }
-            
         }
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
     }
 
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
+    func sceneDidEnterBackground(_ scene: UIScene) { }
 }
 
