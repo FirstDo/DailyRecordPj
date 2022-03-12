78df4523090cf06f6885c9743b05962bfb00337c
dudu <whqtkf12@naver.com>
2022-03-13 07:16:41 +0900

    chore: PlainTableViewCell에 final 키워드 추가 및 주석 삭제

--- DailyRecordProject/Setting/PlainTableViewCell.swift
+++ DailyRecordProject/Setting/PlainTableViewCell.swift
@@ -2,21 +2,17 @@
 //  PlainTableViewCell.swift
 //  DailyRecordProject
 //
-//  Created by 김도연 on 2021/11/16.
+//  Created by DuDu on 2021/11/16.
 //
 
 import UIKit
 
-class PlainTableViewCell: UITableViewCell {
+final class PlainTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
-        // Configure the view for the selected state
     }
-
 }
